ee7d59dd1c435c39f3072607eb880e2059fee047
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2023-03-29 17:24:34 +0300

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+}
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
